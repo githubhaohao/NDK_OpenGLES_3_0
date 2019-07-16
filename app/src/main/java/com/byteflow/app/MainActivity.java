4c5495a42e9669eae7cbdd12c67dbafdc5a85ac1
ByteFlow <haohaochang86@gmail.com>
2019-07-16 15:45:33 +0800

    update

--- app/src/main/java/com/byteflow/app/MainActivity.java
+++ app/src/main/java/com/byteflow/app/MainActivity.java
@@ -73,9 +73,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
