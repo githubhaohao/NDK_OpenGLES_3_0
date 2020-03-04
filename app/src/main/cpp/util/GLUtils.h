ae74ba33711682747f2678aca9a72c292bed55f8
ByteFlow <haohaochang86@gmail.com>
2020-03-04 14:47:06 +0800

    Improve GLUtils

--- app/src/main/cpp/util/GLUtils.h
+++ app/src/main/cpp/util/GLUtils.h
@@ -2,6 +2,8 @@
 #define _BYTE_FLOW_GL_UTILS_H_
 
 #include <GLES3/gl3.h>
+#include <string>
+#include <glm.hpp>
 
 class GLUtils
 {
@@ -19,6 +21,65 @@ public:
 	static void DeleteProgram(GLuint &program);
 
 	static void CheckGLError(const char *pGLOperation);
+
+	// utility uniform functions
+	// ------------------------------------------------------------------------
+	static void setBool(GLuint programId,const std::string &name, bool value)
+	{
+		glUniform1i(glGetUniformLocation(programId, name.c_str()), (int)value);
+	}
+	// ------------------------------------------------------------------------
+	static void setInt(GLuint programId,const std::string &name, int value)
+	{
+		glUniform1i(glGetUniformLocation(programId, name.c_str()), value);
+	}
+	// ------------------------------------------------------------------------
+	static void setFloat(GLuint programId,const std::string &name, float value)
+	{
+		glUniform1f(glGetUniformLocation(programId, name.c_str()), value);
+	}
+	// ------------------------------------------------------------------------
+	static void setVec2(GLuint programId,const std::string &name, const glm::vec2 &value)
+	{
+		glUniform2fv(glGetUniformLocation(programId, name.c_str()), 1, &value[0]);
+	}
+	static void setVec2(GLuint programId,const std::string &name, float x, float y)
+	{
+		glUniform2f(glGetUniformLocation(programId, name.c_str()), x, y);
+	}
+	// ------------------------------------------------------------------------
+	static void setVec3(GLuint programId,const std::string &name, const glm::vec3 &value)
+	{
+		glUniform3fv(glGetUniformLocation(programId, name.c_str()), 1, &value[0]);
+	}
+	static void setVec3(GLuint programId,const std::string &name, float x, float y, float z)
+	{
+		glUniform3f(glGetUniformLocation(programId, name.c_str()), x, y, z);
+	}
+	// ------------------------------------------------------------------------
+	static void setVec4(GLuint programId,const std::string &name, const glm::vec4 &value)
+	{
+		glUniform4fv(glGetUniformLocation(programId, name.c_str()), 1, &value[0]);
+	}
+	static void setVec4(GLuint programId,const std::string &name, float x, float y, float z, float w)
+	{
+		glUniform4f(glGetUniformLocation(programId, name.c_str()), x, y, z, w);
+	}
+	// ------------------------------------------------------------------------
+	static void setMat2(GLuint programId,const std::string &name, const glm::mat2 &mat)
+	{
+		glUniformMatrix2fv(glGetUniformLocation(programId, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+	// ------------------------------------------------------------------------
+	static void setMat3(GLuint programId,const std::string &name, const glm::mat3 &mat)
+	{
+		glUniformMatrix3fv(glGetUniformLocation(programId, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+	// ------------------------------------------------------------------------
+	static void setMat4(GLuint programId,const std::string &name, const glm::mat4 &mat)
+	{
+		glUniformMatrix4fv(glGetUniformLocation(programId, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
 
 };
 #endif // _BYTE_FLOW_GL_UTILS_H_
