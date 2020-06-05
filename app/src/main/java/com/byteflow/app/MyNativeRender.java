82f408999c7d2a59c28b7a326c76df1d3e685351
ByteFlow <haohaochang86@gmail.com>
2020-06-05 15:05:17 +0800

    update

--- app/src/main/java/com/byteflow/app/MyNativeRender.java
+++ app/src/main/java/com/byteflow/app/MyNativeRender.java
@@ -33,6 +33,7 @@ public class MyNativeRender {
     public static final int SAMPLE_TYPE_KEY_VISUALIZE_AUDIO     = SAMPLE_TYPE + 27;
     public static final int SAMPLE_TYPE_KEY_SCRATCH_CARD        = SAMPLE_TYPE + 28;
     public static final int SAMPLE_TYPE_KEY_AVATAR              = SAMPLE_TYPE + 29;
+    public static final int SAMPLE_TYPE_KEY_SHOCK_WAVE          = SAMPLE_TYPE + 30;
 
 
     public static final int SAMPLE_TYPE_SET_TOUCH_LOC           = SAMPLE_TYPE + 999;
