5a6151e74a2d348d25eddae0b95d827439930bf0
ByteFlow <haohaochang86@gmail.com>
2020-08-14 15:32:43 +0800

    Support Android X

--- app/src/main/java/com/byteflow/app/adapter/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/byteflow/app/adapter/MyRecyclerViewAdapter.java
@@ -2,8 +2,8 @@ package com.byteflow.app.adapter;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.*;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
