172c54b4f0246c626fd3dde2501ba3cfec76703d
Chang HaoHao(Hoc) <chh7563@apac.arcsoft.corp>
2020-09-28 18:59:04 +0800

    FBO Blit Sample

---
+++ app/src/main/cpp/sample/FBOBlitSample.h
@@ -0,0 +1,53 @@
+//
+// Created by ByteFlow on 2020/9/30.
+//
+
+#ifndef NDK_OPENGLES_3_0_FBOBLITSAMPLE_H
+#define NDK_OPENGLES_3_0_FBOBLITSAMPLE_H
+
+#include <detail/type_mat.hpp>
+#include <detail/type_mat4x4.hpp>
+#include "GLSampleBase.h"
+#define ATTACHMENT_NUM 4
+class FBOBlitSample : public GLSampleBase
+{
+public:
+	FBOBlitSample();
+
+	virtual ~FBOBlitSample();
+
+	virtual void LoadImage(NativeImage *pImage);
+
+	virtual void Init();
+	virtual void Draw(int screenW, int screenH);
+
+	virtual void Destroy();
+
+	virtual void UpdateTransformMatrix(float rotateX, float rotateY, float scaleX, float scaleY);
+
+	void UpdateMVPMatrix(glm::mat4 &mvpMatrix, int angleX, int angleY, float ratio);
+
+    bool InitFBO();
+
+    void BlitTextures();
+
+private:
+	GLuint m_TextureId;
+    GLuint m_AttachTexIds[ATTACHMENT_NUM];
+    GLuint m_FBO;
+	GLint m_SamplerLoc;
+	GLint m_MVPMatLoc;
+	GLuint m_VaoId;
+	GLuint m_VboIds[3];
+	NativeImage m_RenderImage;
+	glm::mat4 m_MVPMatrix;
+
+	int m_AngleX;
+	int m_AngleY;
+	float m_ScaleX;
+	float m_ScaleY;
+
+};
+
+
+#endif //NDK_OPENGLES_3_0_FBOBLITSAMPLE_H
