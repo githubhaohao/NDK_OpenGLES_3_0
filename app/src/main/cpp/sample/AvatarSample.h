1a1b3d56ac410a6d24d78b6c7e42eb5de38bb0de
字节流动 <haohaochang86@gmail.com>
2021-03-16 15:32:19 +0800

    字节流动

--- app/src/main/cpp/sample/AvatarSample.h
+++ app/src/main/cpp/sample/AvatarSample.h
@@ -1,6 +1,10 @@
-//
-// Created by ByteFlow on 2020/7/30.
-//
+/**
+ *
+ * Created by 公众号：字节流动 on 2020/4/18.
+ * https://github.com/githubhaohao/NDK_OpenGLES_3_0
+ * 最新文章首发于公众号：字节流动，有疑问或者技术交流可以添加微信 Byte-Flow ,领取视频教程, 拉你进技术交流群
+ *
+ * */
 
 #ifndef NDK_OPENGLES_3_0_AVATARSAMPLE_H
 #define NDK_OPENGLES_3_0_AVATARSAMPLE_H
