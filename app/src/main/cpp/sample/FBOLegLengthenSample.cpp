a5d36648bd9522807a0198e0b9071da68c90845e
字节流动 <byteflow@byteflow.com>
2024-11-13 14:59:30 +0800

    update

--- app/src/main/cpp/sample/FBOLegLengthenSample.cpp
+++ app/src/main/cpp/sample/FBOLegLengthenSample.cpp
@@ -1,6 +1,10 @@
-//
-// Created by ByteFlow on 2019/7/16.
-//
+/**
+ *
+ * Created by 公众号：字节流动 on 2022/4/18.
+ * https://github.com/githubhaohao/NDK_OpenGLES_3_0
+ * 最新文章首发于公众号：字节流动，有疑问或者技术交流可以添加微信 Byte-Flow ,领取视频教程, 拉你进技术交流群
+ *
+ * */
 
 #include <GLUtils.h>
 #include <gtc/matrix_transform.hpp>
